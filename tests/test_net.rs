47c8d36b1db6ced56e16eb50f8f6754da3017680
Akita Noek <akita@noek.net>
2025-07-03 07:06:12 -0600

    Double check our expected network status strings are there in test_net

--- tests/test_net.rs
+++ tests/test_net.rs
@@ -15,6 +15,11 @@ fn test_no_network(mut sandbox: SandboxManager) -> Result<()> {
         "1",
         "127.0.0.1"
     ]));
+    assert!(
+        sandbox
+            .last_stderr
+            .contains("Creating new network namespace")
+    );
     Ok(())
 }
 
@@ -29,6 +34,7 @@ fn test_network(mut sandbox: SandboxManager) -> Result<()> {
         "1",
         "127.0.0.1",
     ])?;
+    assert!(sandbox.last_stderr.contains("Using host network"));
     assert!(status.status.success());
     Ok(())
 }
